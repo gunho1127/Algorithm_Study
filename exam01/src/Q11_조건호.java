4d42ffa46354e125e8d856e94d157c7feaf2af36
gunho11127 <rjsghsky@gmail.com>
2024-10-10 15:35:27 +0900

    구구단

--- exam01/src/Q11_조건호.java
+++ exam01/src/Q11_조건호.java
@@ -13,5 +13,17 @@ public class Q11_조건호 {
             System.out.println();
         }
 
+//        for (int i = 1; i <= 9 ; i++) {
+//            System.out.print(" ");
+//            for (int j = 1; j <= 9 ; j++) {
+//                if (i*j < 10) {
+//                    System.out.print(" " + i*j + " ");
+//                } else {
+//                    System.out.print(i*j + " ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
     }
 }
