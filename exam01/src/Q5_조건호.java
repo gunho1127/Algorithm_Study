0c7c23453c9c2fe49fc97bfa2b1a83d4ad5ede51
gunho11127 <rjsghsky@gmail.com>
2024-10-10 13:13:43 +0900

    for 문으로 1,2, ... ,n의 합을 구함

---
+++ exam01/src/Q5_조건호.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Q5_조건호 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int n;
+        int sum = 0;
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.print("n값: ");
+        n = scan.nextInt();
+
+        for (int i = 1;i <= n;i++){
+            sum += i;
+        }
+
+        System.out.print("1부터" + n +"까지의 합은 " + sum + " 입니다.");
+
+    }
+}
