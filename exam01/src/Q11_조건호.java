df16a28db38533b6729e222b61994accae52e2f3
gunho11127 <rjsghsky@gmail.com>
2024-10-10 15:31:27 +0900

    구구단 출력

---
+++ exam01/src/Q11_조건호.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q11_조건호 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("----- 구구단 곱셈표 -----");
+
+        for (int i = 1; i <= 9 ; i++) {
+            System.out.print(" ");
+            for (int j = 1; j <= 9 ; j++) {
+                if (i*j < 10) {
+                    System.out.print(" " + i*j + " ");
+                } else {
+                    System.out.print(i*j + " ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
