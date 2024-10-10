44b05983ef34243b6de8d6759fafed32858213ec
gunho11127 <rjsghsky@gmail.com>
2024-10-10 15:34:05 +0900

    구구단

--- exam01/src/Q11_조건호.java
+++ exam01/src/Q11_조건호.java
@@ -6,14 +6,9 @@ public class Q11_조건호 {
 
         System.out.println("----- 구구단 곱셈표 -----");
 
-        for (int i = 1; i <= 9 ; i++) {
-            System.out.print(" ");
-            for (int j = 1; j <= 9 ; j++) {
-                if (i*j < 10) {
-                    System.out.print(" " + i*j + " ");
-                } else {
-                    System.out.print(i*j + " ");
-                }
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.printf("%3d", i * j);
             }
             System.out.println();
         }
